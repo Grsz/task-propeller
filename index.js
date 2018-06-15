46d96fb904d8cf9e675b43908a6f200bf369146c
Grsz <hrgergsz@gmail.com>
2018-06-15 14:00:59 +0200

    created js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 fetch('https://design.propcom.co.uk/buildtest/accordion-data.json')
     .then(res => res.json())
-    .then(blocks => {
-        
+    .then(data => {
+        const blocks = data.blocks;
+        const headings = document.querySelectorAll(".block > h1");
+        const paragraphs = document.querySelectorAll(".block > p")
+        blocks.forEach((block, i) => {
+            const heading = document.createTextNode(block.heading);
+            const paragraph = document.createTextNode(block.content);
+
+            headings[i].appendChild(heading);
+            paragraphs[i].appendChild(paragraph);
+
+            headings[i].onclick = function(){
+                paragraphs[i].classList.toggle("visible")
+            }
+        });
     })
     .catch(err => console.log(err))
