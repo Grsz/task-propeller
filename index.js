6ce81ab0ed8070c9ef5943fd8d9b3c98121b99a0
Grsz <hrgergsz@gmail.com>
2018-06-15 19:06:22 +0200

    did some changes. Reason: improved flexibility for possible content changing

--- index.js
+++ index.js
@@ -3,22 +3,44 @@ fetch('https://design.propcom.co.uk/buildtest/accordion-data.json')
     .then(data => {
 
         const blocks = data.blocks;
-        const headingWrappers = document.querySelectorAll(".block > .headingWrapper");
-        const contentWrappers = document.querySelectorAll(".block > .contentWrapper");
-        const headings = document.querySelectorAll(".block > .headingWrapper > h1");
-        const contents = document.querySelectorAll(".block > .contentWrapper > p");
+        const main = document.querySelector("main");
 
         blocks.forEach((block, i) => {
+            const section = document.createElement("SECTION");
 
-            const heading = document.createTextNode(block.heading);
-            const content = document.createTextNode(block.content);
+            const headingWrapper = document.createElement("DIV");
+            const contentWrapper = document.createElement("DIV");
 
-            headings[i].appendChild(heading);
-            contents[i].appendChild(content);
+            headingWrapper.classList.add("headingWrapper");
+            contentWrapper.classList.add("contentWrapper");
 
-            headingWrappers[i].onclick = function(){
-                contentWrappers[i].classList.toggle("visible")
-                headingWrappers[i].classList.toggle("open")
+            const heading = document.createElement("H1");
+            const content = document.createElement("P");
+
+            const headingText = document.createTextNode(block.heading);
+            const contentText = document.createTextNode(block.content);
+
+            const arrow = document.createElement("IMG");
+            arrow.src = "img.png";
+            arrow.alt = "arrow";
+
+
+            heading.appendChild(headingText);
+            content.appendChild(contentText);
+
+            headingWrapper.appendChild(heading);
+            headingWrapper.appendChild(arrow);
+            contentWrapper.appendChild(content);
+
+            section.appendChild(headingWrapper);
+            section.appendChild(contentWrapper);
+
+            main.appendChild(section)
+
+
+            headingWrapper.onclick = function(){
+                contentWrapper.classList.toggle("visible")
+                headingWrapper.classList.toggle("open")
             }
         });
     })
